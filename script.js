674066bdc870ffc5a679e8d1aad265c7f23f5f8f
Saiful123ss <156865701+Saiful123ss@users.noreply.github.com>
2025-08-24 13:26:03 +0800

    draft 8

--- script.js
+++ script.js
@@ -3,15 +3,9 @@ const overlay = document.getElementById('overlay');
 const closeBtn = document.getElementById('closeBtn');
 
 surpriseBtn.addEventListener('click', () => {
-  overlay.style.display = 'flex';
-  setTimeout(() => {
-    overlay.classList.add('show');
-  }, 200);
+  overlay.classList.add('show');
 });
 
 closeBtn.addEventListener('click', () => {
   overlay.classList.remove('show');
-  setTimeout(() => {
-    overlay.style.display = 'none';
-  }, 500);
 });
